92069d82f1bc9e0c44708b71f2ea2e266a4e96da
user <sean-ThinkPad-T420>
2014-03-22 14:47:44 -0400

    yacs change

--- Connective/yacs.js
+++ Connective/yacs.js
@@ -1,133 +1,135 @@
 var http=require("http");
 var fs=require("fs"); // Include filesystem manipulation, so we can read/write files for cacheing.
 
-/* The getCourses()  function takes a callback function which is passed the resulting JSON from YACS. */
+var MILLISECONDS_IN_A_WEEK = 1000*60*60*24*7;
+
+/* The getCourses() function takes a callback function which is passed the resulting JSON from YACS. */
 function getCourses(callback) {
-
-  /* First, check if the cached file is older than 1 week. If not, just use that data. */
-  var lastUpdated=-1; // If there's no cache yet, this will ensure it's processed as "too old" and be re-cached from YACS.
-  if (fs.existsSync("caches/courseCache.json")) {
-    var lastUpdated = fs.lstatSync("caches/courseCache.json").mtime; // Get the file's last-modified time.
-  }
-  if (new Date() - lastUpdated < 1000*60*60*24*7) { // Convert elapsed milliseconds to weeks and compare
-    callback(JSON.parse(fs.readFileSync("caches/courseCache.json"))); // Pass the cached data to the callback.
-  }
-  else {
-  /* If the cache is too old, reload it from the YACS server */
-  
-    /* Set the HTTP request parameters */
-    var requestOptions={
-      hostname: "yacs.me",
-      post:80,
-      path: "/api/4/courses/",
-      method: "GET"
-    };
-    
-    /* Request the JSON file */
-    var request=http.request(requestOptions, function (response) {
-      
-      /* COncatenate the response as data comes in */
-      response.body="";
-      response.on('data', function (chunk) {
-        response.body+=chunk;
-      });
-      
-      /* When all data has been received, cache it and pass it to the callback function */
-      response.on("end", function() {
-        fs.writeFile("caches/courseCache.json", response.body, null); // Cache the data
-        callback(JSON.parse(response.body));
-      });
-    });
-    
-    /* Send the request */
-    request.end();
-  }
+  getLocallyOrGetFromWebAndCacheForAWeek("course",callback);
 }
 
-/* The getDepartments() function works basically the same way as getCourses() */
 function getDepartments(callback) {
-
-  var lastUpdated=-1;
-  if (fs.existsSync("caches/departmentCache.json")) {
-    var lastUpdated = fs.lstatSync("caches/departmentCache.json").mtime;
-  }
-  if (new Date() - lastUpdated < 1000*60*60*24*7) {
-    callback(JSON.parse(fs.readFileSync("caches/departmentCache.json")));
-  }
-  else {
-    
-    var requestOptions={
-      hostname: "yacs.me",
-      post:80,
-      path: "/api/4/departments/",
-      method: "GET"
-    };
-    
-    var request=http.request(requestOptions, function (response) {
-      
-      response.body="";
-      response.on('data', function (chunk) {
-        response.body+=chunk;
-      });
-      
-      response.on("end", function() {
-        fs.writeFile("caches/departmentCache.json", response.body, null);
-        callback(JSON.parse(response.body));
-      });
-    });
-    
-    /* Send the request */
-    request.end();
-  }
+  getLocallyOrGetFromWebAndCacheForAWeek("department",callback);
 }
 
-/* And getSections() for class sections. Yeah... same way. */
 function getSections(callback) {
-
-  var lastUpdated=-1;
-  if (fs.existsSync("caches/sectionCache.json")) {
-    var lastUpdated = fs.lstatSync("caches/sectionCache.json").mtime;
-  }
-  if (new Date() - lastUpdated < 1000*60*60*24*7) {
-    callback(JSON.parse(fs.readFileSync("caches/sectionCache.json")));
-  }
-  else {
-    
-    var requestOptions={
-      hostname: "yacs.me",
-      post:80,
-      path: "/api/4/sections/",
-      method: "GET"
-    };
-    
-    var request=http.request(requestOptions, function (response) {
-      
-      response.body="";
-      response.on('data', function (chunk) {
-        response.body+=chunk;
-      });
-      
-      response.on("end", function() {
-        fs.writeFile("caches/sectionCache.json", response.body, null);
-        callback(JSON.parse(response.body));
-      });
-    });
-    
-    /* Send the request */
-    request.end();
-  }
+  getLocallyOrGetFromWebAndCacheForAWeek("section",callback);
 }
 
-/* The getSemesters() function takes a callback function which is passed the resulting JSON from YACS. */
 function getSemesters(callback) {
+  getLocallyOrGetFromWebAndCacheForAWeek("semester",callback);
+}
 
+function getDataOfThisType(typeOfYacsData, callback) {
+  getLocallyOrGetFromWebAndCacheForAWeek(typeOfYacsData, callback);
+}
+
+function computeClassListings(coursesData,departmentsData,sectionsData,semestersData)
+{
+    
+}
+
+function getAllClassListings(callback) {
+  getCourses(function(coursesData){
+  getDepartments(function(departmentsData){
+  getSections(function(sectionsData){
+  getSemesters(function(semestersData){
+  
+        var lastUpdated = 0;
+        if (fs.existsSync("caches/classListings.json")) {
+             var lastUpdated = fs.lstatSync("caches/classListings.json").mtime;
+        }
+        if (new Date() - lastUpdated < MILLISECONDS_IN_A_WEEK) {
+             callback(JSON.parse(fs.readFileSync("caches/classListings.json"))); 
+        }
+
+	var courses = coursesData["result"];
+	var departments = departmentsData["result"];
+	var sections = sectionsData["result"]; 
+        var semesters = semestersData["result"];
+        semesters.sort(function(s1,s2){
+          var t1 = new Date(s1.year, s1.month).getTime();
+          var t2 = new Date(s2.year, s2.month).getTime();
+          return t2-t1;
+        });
+        var lastTwoSemesterNames = [semesters[0]["name"], semesters[1]["name"]];
+        var lastTwoSemesterIds = [semesters[0]["id"],semesters[1]["id"]];
+        //console.log(lastTwoSemesters);
+	var departmentIdsToCodes = {};
+        
+        var classListings = [];
+	for (var i = 0; i < departments.length; i++)
+	{
+	  var departmentId = departments[i]["id"];
+	  var departmentCode = departments[i]["code"];
+	  departmentIdsToCodes[departmentId.toString()] = departmentCode;
+	} 
+	var classIdsToListOfSections = {};
+	for (var i = 0; i < sections.length; i++)
+	{
+	  //make this number a fucking constant at the top
+	  //if (sections[i]["semester_id"] != 16073)
+         
+          var semesterIndexOfThisSection = lastTwoSemesterIds.indexOf(sections[i]["semester_id"]);
+          //if (lastTwoSemesters.indexOf(sections[i]["semester_id"]) == -1)
+          if (semesterIndexOfThisSection == -1)
+	    continue; //sections are not for two semesters of Interest (this and next, or this and last)
+
+	  var classIdAsString = sections[i]["course_id"].toString();
+
+	  var sectionForAClass =  sections[i]["number"]
+	  /*for (var j = 0; j < sectionsForAClass.length; j++)
+	  {
+	    //var sectionInfo =  sectionsForAClass["start"];
+	    classIdsToListOfSections[classIdAsString]
+	        = sections[i]["section_times"];
+	  }*/
+	if (classIdsToListOfSections[classIdAsString] == null)
+	  classIdsToListOfSections[classIdAsString] 
+                  = [{section:sectionForAClass, semester:lastTwoSemesterNames[semesterIndexOfThisSection]}];
+	else classIdsToListOfSections[classIdAsString]
+                  .push({section:sectionForAClass, semester:lastTwoSemesterNames[semesterIndexOfThisSection]});
+	}
+	//console.log("classIdsToListOfSections num fields = " 
+	//	+ classIdsToListOfSections.__count__);
+	//console.log(classIdsToListOfSections);
+	for (var i = 0; i < courses.length; i++)
+        {
+         var classIdAsString = courses[i]["id"].toString();
+         var sectionsOfThisClassThisSemester = classIdsToListOfSections[classIdAsString];
+         if (sectionsOfThisClassThisSemester == null)
+           continue;//class doesn't have any sections this semester
+         var departmentCodeOfClass = 
+             departmentIdsToCodes[courses[i]["department_id"].toString()];
+         var numberOfClass = courses[i]["number"];
+         var nameOfClass = courses[i]["name"];
+         for (var j = 0; j < sectionsOfThisClassThisSemester.length; j++)
+         {
+           var sectionNumber = sectionsOfThisClassThisSemester[j].section;
+           var semester = sectionsOfThisClassThisSemester[j].semester;
+           var classListing = departmentCodeOfClass + " " 
+                  + numberOfClass + "-" + sectionNumber + ":" + nameOfClass + "--" + semester;
+           classListings.push(classListing);     
+         }     
+        }  
+        fs.writeFile("caches/classListings",JSON.stringify({classes:classListings}));
+        callback({classes:classListings});
+  });
+  });
+  });
+  });
+}
+
+function getLocallyOrGetFromWebAndCacheForAWeek(thingToGet, callback){
+  var thingToGetPlural = thingToGet + "s";
+  
   /* First, check if the cached file is older than 1 week. If not, just use that data. */
-  var lastUpdated=-1; // If there's no cache yet, this will ensure it's processed as "too old" and be re-cached from YACS.
-  if (fs.existsSync("caches/semesterCache.json")) {
-    var lastUpdated = fs.lstatSync("caches/semesterCache.json").mtime; // Get the file's last-modified time.
+  var lastUpdated=0; // If there's no cache yet, this will ensure it's processed as "too old" and be re-cached from YACS.
+  if (fs.existsSync("caches/" + thingToGet + "Cache.json")) {
+    var lastUpdated = fs.lstatSync("caches/" + thingToGet + "Cache.json").mtime; // Get the file's last-modified time.
   }
-  if (new Date() - lastUpdated < 1000*60*60*24*7) { // Convert elapsed milliseconds to weeks and compare
-    callback(JSON.parse(fs.readFileSync("caches/semesterCache.json"))); // Pass the cached data to the callback.
+  if (new Date() - lastUpdated < MILLISECONDS_IN_A_WEEK) { // Convert elapsed milliseconds to weeks and compare
+    callback(JSON.parse(fs.readFileSync("caches/" + thingToGet + "Cache.json"))); // Pass the cached data to the callback.
   }
   else {
   /* If the cache is too old, reload it from the YACS server */
@@ -136,7 +138,7 @@ function getSemesters(callback) {
     var requestOptions={
       hostname: "yacs.me",
       post:80,
-      path: "/api/4/semesters/",
+      path: "/api/4/" + thingToGetPlural + "/",
       method: "GET"
     };
     
@@ -151,7 +153,7 @@ function getSemesters(callback) {
       
       /* When all data has been received, cache it and pass it to the callback function */
       response.on("end", function() {
-        fs.writeFile("caches/semesterCache.json", response.body, null); // Cache the data
+        fs.writeFile("caches/" + thingToGet + "Cache.json", response.body, null); // Cache the data
         callback(JSON.parse(response.body));
       });
     });
@@ -160,8 +162,13 @@ function getSemesters(callback) {
     request.end();
   }
 }
+
 
 /* Make sure the functions can be called if this is used as a Node module...which it is. */
 module.exports.getCourses=getCourses;
 module.exports.getDepartments=getDepartments;
 module.exports.getSections=getSections;
+module.exports.getSemesters=getSemesters;
+
+module.exports.getDataOfThisType=getDataOfThisType;
+module.exports.getAllClassListings=getAllClassListings;
